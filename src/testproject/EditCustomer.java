817d4a5da049cd7fa90c2938d926b8991d0251a8
Sanjoy De <sanjoy.de.it@gmail.com>
2024-02-20 18:22:33 +0000

    SecondCustomer.java file added and EditCustomer.java file modified

--- src/testproject/EditCustomer.java
+++ src/testproject/EditCustomer.java
@@ -1,2 +1,5 @@
 //This is a new file created by second person
 //git pull will download this file in the First person's local based
+
+
+//Few code changes made in this file
---
+++ src/testproject/SecondCustomer.java
@@ -0,0 +1,9 @@
+package testproject;
+
+public class SecondCustomer {
+	//Another java file added into this project
+	
+	void diaplsy() {
+		System.out.println("Nothis to display ..............");
+	}
+}
