833e50e3093715a82da63d70fd1fc2a9bd5b429f
sanjoyde <sanjoy.de.it@gmail.com>
2024-02-20 18:25:49 +0000

    Update SecondCustomer.java

--- src/testproject/SecondCustomer.java
+++ src/testproject/SecondCustomer.java
@@ -4,6 +4,6 @@ public class SecondCustomer {
 	//Another java file added into this project
 	
 	void diaplsy() {
-		System.out.println("Nothis to display ..............");
+		System.out.println("There are thins to diaplsy to display ..............");
 	}
 }
