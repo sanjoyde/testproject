49572f99e28bfbb9c5a7703a977f9008a0528cec
Sanjoy De <sanjoy.de.it@gmail.com>
2024-02-20 17:36:01 +0000

    my first commit ....

---
+++ src/testproject/FirstTest.java
@@ -0,0 +1,13 @@
+package testproject;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		System.out.println("-----------------------");
+		System.out.println("This is my test project");
+	}
+
+}
